b13039d81cf3ba23bcbd534d21cf55bacb5acb08
mariavmujicau <mariavmujicau@gmail.com>
2023-03-03 12:35:21 +0000

    Premier Commit

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<header>
+<?php 
+$page =$_SERVER['PHP_SELF']; 
+?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">LA GALERIE <br>DU LUXE</a>
@@ -6,14 +10,14 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
+        <li <?php if(strpos($page,"index.php")) echo'class="active"'; ?> >
+       <a href="index.php">Accueil</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="listing.php">Nos articles</a>
+        <li <?php if(strpos($page,"listing.php")) echo'class="active"'; ?>>
+          <a  href="listing.php">Nos articles</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="form_recherche.php">Formulaire</a>
+        <li <?php if(strpos($page,"form_recherche.php")) echo'class="active"'; ?>>
+          <a href="form_recherche.php">Formulaire</a>
         </li>
           <!-- <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Action</a></li>
@@ -26,3 +30,4 @@
     </div>
   </div>
 </nav>
+</header>
