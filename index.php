985198b364ab8868be48d6f165d85ffa7e60334e
mariavmujicau <mariavmujicau@gmail.com>
2023-03-20 08:56:45 +0000

    demo lundi 20/03

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require ('header.php');
     <div id="baniere">
     <button>Articles</button>
     </div>
-    <p id="bandesc">Articles des marques les plus iconiques dans l'industrie de la mode...</p>
+    <p id="bandesc">Articles des marques les plus iconiques dans l'industrie de la mode !...</p>
     </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
